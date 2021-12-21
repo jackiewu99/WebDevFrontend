7d5dbdd536f52cc8078edac132fe638e3a8f0a11
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2021-12-20 20:47:29 -0500

    starter code

---
+++ src/components/views/AllCampusesView.js
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const AllCampusesView = (props) => {
+  const {campuses, deleteCampus} = props;
+
+  if (!props.allCampuses.length) {
+    return <div>There are no campuses.</div>;
+  }
+
+  return (
+    <div>
+      {props.allCampuses.map((campus) => (
+        <div key={campus.id}>
+          <Link to={`/campus/${campus.id}`}>
+            <h1>{campus.name}</h1>
+          </Link>
+          
+          <p>{campus.description}</p>
+          <button onClick={() => deleteCampus(campus.id)}>Delete</button>
+        </div>
+      ))}
+
+<Link to={`/newcampus`}>
+        <button>Add New Campus</button>
+      </Link>
+    </div>
+  );
+};
+
+AllCampusesView.propTypes = {
+  allCampuses: PropTypes.array.isRequired,
+};
+
+export default AllCampusesView;

df62702a6570c8d48dcded141748b520b427aa29
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2021-12-20 21:03:43 -0500

    clicking on name in single-campus view redirects

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 const CampusView = (props) => {
   const {campus} = props;
@@ -12,7 +12,11 @@ const CampusView = (props) => {
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
         return (
-          <li key={student.id}>{name}</li>
+          <li key={student.id}>
+          <Link to={`/student/${student.id}`}>
+          {name}</Link>
+          </li>
+          
         );
       })}
       </ul>
